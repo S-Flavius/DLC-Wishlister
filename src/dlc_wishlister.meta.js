c58250e3bbd3a8aa8306f066d27a0160383bc157
S-Flavius <76589861+S-Flavius@users.noreply.github.com>
2022-11-27 23:47:12 +0100

    Create dlc_wishlister.meta.js

---
+++ src/dlc_wishlister.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         DLC wishlister
+// @namespace    https://github.com/S-Flavius/DLC-Wishlister
+// @version      1.0
+// @description  Automatically adds DLC for owned games to your wishlist
+// @author       S-Flavius
+// @match        https://store.steampowered.com/app/*
+// @updateURL    https://github.com/S-Flavius/DLC-Wishlister/raw/main/dlc_wishlister.meta.js
+// @downloadURL  https://github.com/S-Flavius/DLC-Wishlister/raw/main/dlc_wishlister.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// @grant        none
+// ==/UserScript==
